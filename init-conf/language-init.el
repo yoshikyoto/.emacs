cd0f01bf393f179734875bb068fe461ec61f0c71
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2016-08-05 17:02:05 +0900

    languageの設定を分けた

---
+++ init-conf/language-init.el
@@ -0,0 +1,14 @@
+;; 言語に関する設定
+;; TODO この辺の設定必要なのか吟味は必要
+(set-language-environment 'Japanese)
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(aset char-width-table #x25CB 2)
+(aset char-width-table #x25CE 2)
+(aset char-width-table #x25CF 2)
+(aset char-width-table #x301C 2)
+(aset char-width-table #x2606 2)
+(aset char-width-table #x25A0 2)
+(aset char-width-table #x25A1 2)
+(aset char-width-table #x5294 2)
+(aset char-width-table #x7981 2)
--- init.el
+++ init.el
@@ -26,19 +26,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; 言語と文字コード
-;; TODO 必要なのか
-(set-language-environment 'Japanese)
-(prefer-coding-system 'utf-8)
-(set-default-coding-systems 'utf-8)
-(aset char-width-table #x25CB 2)
-(aset char-width-table #x25CE 2)
-(aset char-width-table #x25CF 2)
-(aset char-width-table #x301C 2)
-(aset char-width-table #x2606 2)
-(aset char-width-table #x25A0 2)
-(aset char-width-table #x25A1 2)
-(aset char-width-table #x5294 2)
-(aset char-width-table #x7981 2)
+(load "language-init")
 
 ;; カーソルのある行がハイライトされる
 ;; (global-hl-line-mode t)
