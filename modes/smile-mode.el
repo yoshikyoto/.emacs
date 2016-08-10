8cc254fd20df651b8de49b5cee294c4626f8729b
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2016-08-10 18:45:18 +0900

    便利コマンド追加

--- modes/smile-mode.el
+++ modes/smile-mode.el
@@ -29,5 +29,43 @@
   (interactive)
   (async-shell-command "cd ~/smile/vagrant && vagrant halt"))
 
+;; エラーログをtail
+(defun smile-local-critical-log-tail()
+  (interactive)
+  (async-shell-command
+   "ssh smile@smile-local 'tail -f /var/log/smilevideo/critical.log'"))
+
+(defun smile-local-info-log-tail()
+  (interactive)
+  (async-shell-command
+   "ssh smile@smile-local 'tail -f /var/log/smilevideo/info.log'"))
+
+(defun smile-local-warning-log-tail()
+  (interactive)
+  (async-shell-command
+   "ssh smile@smile-local 'tail -f /var/log/smilevideo/warning.log'"))
+
+(defun smile-local-notice-log-tail()
+  (interactive)
+  (async-shell-command
+   "ssh smile@smile-local 'tail -f /var/log/smilevideo/notice.log'"))
+
+(defun smile-local-debug-log-tail()
+  (interactive)
+  (async-shell-command
+   (concat "ssh smile@smile-local 'tail -f /var/log/smilevideo/debug.log'")))
+
+(defun smile-local-queue-restart()
+  (interactive)
+  (async-shell-command
+   "ssh smile@smile-local 'cd smilevideo && export SMILE_ENV=local && php artisan queue:restart'"))
+
+(defun smile-local-polling-job-start()
+  (interactive)
+  (async-shell-command
+   (concat
+    "ssh smile@smile-local "
+    "'sudo /sbin/initctl start smilevideo-dmc-polling-queue-worker-boot && "
+    "sudo /sbin/initctl start smilevideo-dmc-upload-queue-worker-boot'")))
 
 ;;; smile-mode.el ends here
