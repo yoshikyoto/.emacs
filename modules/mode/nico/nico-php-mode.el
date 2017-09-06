200ddaef3e1806b06853dea52316dd67acfac005
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2017-09-06 09:45:24 +0900

    nico-php-modeがうまく動いていなかったので修正

--- modules/mode/nico/nico-php-mode.el
+++ modules/mode/nico/nico-php-mode.el
@@ -12,11 +12,11 @@
  " Nico" ;モードラインに表示される文字列
  nil)
 
-;; hook 関数の定義（モード切り替え時に呼ばれる）
- (defun nico-php-mode-hooks ()
-   (setq indent-tabs-mode t) ;インデントはタブに変更
-   (setq c-basic-offset 4) ;タブ幅を4に設定
-   (c-set-offset 'case-label '+)) ;switch-caseでインデントする
-
 ;; hookを登録
-(add-hook 'nico-php-mode-hook 'nico-php-mode-hooks)
+(add-hook
+ 'nico-php-mode-hook
+ '(lambda ()
+    (setq indent-tabs-mode t) ;インデントにはタブを使用
+    (setq c-basic-offset 4) ;タブ幅を4に設定
+    (c-set-offset 'case-label '+) ;switch-caseでインデントする
+    ))
