ae68e4ae324b92d793fc57e66c9e54ea8fe250ab
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2018-10-17 16:58:37 +0900

    php便利関数追加

--- modules/local/php/code.el
+++ modules/local/php/code.el
@@ -1,10 +1,13 @@
+;;; phpのcodeを解釈するのに役に立つ関数
+
+;; 今カーソルがいるpublic methodの名前を取得する
 (defun php-current-public-method-name ()
-  (interactive)
-  (search-backward "public function")
-  (search-forward " function ")
-  (setq method-name-start (point))
-  (search-forward "(")
-  (left-char)
-  (setq method-name-end (point))
+  (search-backward "public function") ; カーソルから一番近い"public function"を探す
+  (search-forward " function ") ; " function " の後ろの部分にカーソルを移動
+  (setq method-name-start (point)) ; ここをメソッド名のスタートとする
+  (search-forward "(") ; "(" を探す
+  (left-char) ; "(" の一つ前にカーソルを移動
+  (setq method-name-end (point)) ; ここがメソッド名の終わりとする
+  ;; 最後に文字列を切り出した返す
   (string-trim (buffer-substring-no-properties method-name-start method-name-end))
   )
