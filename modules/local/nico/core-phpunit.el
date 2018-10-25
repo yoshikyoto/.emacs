1c5cedfb67154e378d028448bbd41cc502acd85c
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2018-10-25 21:07:23 +0900

    replace mockery

--- modules/local/nico/core-phpunit.el
+++ modules/local/nico/core-phpunit.el
@@ -5,3 +5,28 @@
     "cd ~ && "
     "phpunit --bootstrap=core/test/phpunit/phpunit.php "
     buffer-file-name)))
+
+(defun replace-mockery ()
+  (interactive)
+  (beginning-of-buffer)
+  (replace-string "$this->getMock('" "m::mock(\\")
+  (beginning-of-buffer)
+  (replace-string "$this->getMock(" "m::mock(")
+  (beginning-of-buffer)
+  (replace-string "$this->getMockBuilder('" "m::mock(\\")
+
+  (beginning-of-buffer)
+  (replace-string "->method(" "->shouldReceive(")
+
+  (beginning-of-buffer)
+  (replace-string "->will($this->throwException(" "->andThrow(")
+  (beginning-of-buffer)
+  (replace-string "->will($this->returnValue(" "->andReturn(")
+  (beginning-of-buffer)
+  (replace-string "->will($this->returnCallback(" "->andReturn(")
+  (beginning-of-buffer)
+  (replace-string "->will(" "->andReturn(")
+
+  (beginning-of-buffer)
+  (replace-string "$this->equalTo(" "")
+  )
