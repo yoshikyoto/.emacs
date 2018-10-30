5017309721d40f9beaf193872b52e2ea1e2536c6
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2018-10-30 12:53:02 +0900

    mockery置き換えメソッドの進化

--- modules/local/nico/core-phpunit.el
+++ modules/local/nico/core-phpunit.el
@@ -8,25 +8,58 @@
 
 (defun replace-mockery ()
   (interactive)
-  (beginning-of-buffer)
-  (replace-string "$this->getMock('" "m::mock(\\")
-  (beginning-of-buffer)
-  (replace-string "$this->getMock(" "m::mock(")
+
+  (replace-mockery-getmock)
   (beginning-of-buffer)
   (replace-string "$this->getMockBuilder('" "m::mock(\\")
 
   (beginning-of-buffer)
   (replace-string "->method(" "->shouldReceive(")
 
-  (beginning-of-buffer)
-  (replace-string "->will($this->throwException(" "->andThrow(")
-  (beginning-of-buffer)
-  (replace-string "->will($this->returnValue(" "->andReturn(")
+  (replace-mockery-throw)
+  (replace-mockery-returnvalue)
   (beginning-of-buffer)
   (replace-string "->will($this->returnCallback(" "->andReturn(")
   (beginning-of-buffer)
   (replace-string "->will(" "->andReturn(")
 
   (beginning-of-buffer)
+  (replace-string "->expects($this->any())" "")
+  (replace-mockery-expects)
+
+  (beginning-of-buffer)
   (replace-string "$this->equalTo(" "")
   )
+
+;; $this->getMock() を Mockeryに置き換えていく
+(defun replace-mockery-getmock ()
+  (interactive)
+  ;;  $this->getMock('Database', ['get_count_of_user']) みたいなのを置き換える
+  (beginning-of-buffer)
+  (replace-regexp "$this->getMock('\\([^(),]+\\)',[^()]+)" "m::mock(\\1::class)")
+
+  ;; $this->getMock('Database') みたいなのを置き換える
+  (beginning-of-buffer)
+  (replace-regexp "$this->getMock('\\([^()]+\\)')" "m::mock(\\1::class)")
+
+  ;; それ以外はまとめて置き換える
+  (beginning-of-buffer)
+  (replace-string "$this->getMock(" "m::mock("))
+
+;; mockeryのandReturnに置き換えていく
+(defun replace-mockery-returnvalue ()
+  (interactive)
+  (beginning-of-buffer)
+  (replace-regexp "->will($this->returnValue(\\([^()]+\\)))" "->andReturn(\\1)"))
+
+;; mockeryのandThorwに置き換えていく
+(defun replace-mockery-throw ()
+  (interactive)
+  (beginning-of-buffer)
+  (replace-regexp "->will($this->throwException(\\([^()]+\\)))" "->andThrow(\\1)"))
+
+;; once() とか times() に置き換える
+(defun replace-mockery-expects ()
+  (interactive)
+  (beginning-of-buffer)
+  (replace-regexp "->expects($this->once())->shouldReceive(\\([^()]+\\))" "->shouldReceive(\\1)->once()"))
