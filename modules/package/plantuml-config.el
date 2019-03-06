655b2cc395c9507af5273a9a8acf5682be5d3593
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2019-03-06 09:38:35 +0900

    plantuml config

---
+++ modules/package/plantuml-config.el
@@ -0,0 +1,11 @@
+;;; plantuml-config -- plantumlの設定を書く
+
+;;; Commentary:
+;; plantumlについて環境依存の設定を置く場所
+;; 詳細は plantuml.el の方を参照
+
+;;; Code:
+;; plantumlコマンドの実行jarファイルの場所
+(setq plantuml-jar-path "/usr/local/Cellar/plantuml/1.2019.1/libexec/plantuml.jar")
+
+;;; plantuml-config.el ends here
