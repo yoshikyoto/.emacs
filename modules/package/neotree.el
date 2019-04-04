1a9ab24621dbf5471ebdde0504ec7f5278640aed
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2019-04-04 18:55:21 +0900

    update neotree config

--- modules/package/neotree.el
+++ modules/package/neotree.el
@@ -8,13 +8,19 @@
 (el-get-bundle neotree)
 ;; F8でnetreee-windowが開くようにする
 (global-set-key [f8] 'neotree-toggle)
+
 ;; neotreeでファイルを新規作成した場合のそのファイルを開く
 (setq neo-create-file-auto-open t)
+
 ;; delete-other-window で neotree ウィンドウを消さない
 (setq neo-persist-show t)
+
 ;; C-x }, C-x { でwindowサイズを変更できるよにする
 ;; でもなんか事あるごとに幅がリセットされる
 (setq neo-window-fixed-size nil)
+
+;; neotree ウィンドウを表示する毎に current file のあるディレクトリを表示する
+(setq neo-smart-open nil)
 
 ;; キーバインドをシンプルにする
 ;; C: ルートディレクトリの変更
